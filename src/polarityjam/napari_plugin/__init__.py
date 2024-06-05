72b58d11a1f2d6e448d40d370726b74c305e8955
Jan Philipp Albrecht <j.p.albrecht@fu-berlin.de>
2024-06-05 14:36:11 +0200

    bump version 0.1.1

--- src/polarityjam/napari_plugin/__init__.py
+++ src/polarityjam/napari_plugin/__init__.py
@@ -1,3 +1,4 @@
+"""PolarityJam napari plugin."""
 __import__("pkg_resources").declare_namespace(__name__)
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 __author__ = "Jan Philipp Albrecht, Wolfgang Giese"
