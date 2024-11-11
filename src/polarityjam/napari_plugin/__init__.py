40f3cc19082988a5a7573f78d2a3171ead2dc12f
Jan Philipp Albrecht <j.p.albrecht@fu-berlin.de>
2024-11-11 10:21:53 +0100

    bump new version

--- src/polarityjam/napari_plugin/__init__.py
+++ src/polarityjam/napari_plugin/__init__.py
@@ -1,4 +1,4 @@
 """PolarityJam napari plugin."""
 __import__("pkg_resources").declare_namespace(__name__)
-__version__ = "0.1.1"
+__version__ = "0.2.0"
 __author__ = "Jan Philipp Albrecht, Wolfgang Giese"
--- src/polarityjam/napari_plugin/__main__.py
+++ src/polarityjam/napari_plugin/__main__.py
@@ -1,9 +1,11 @@
-from polarityjam.napari_plugin.startup import startup
+"""This module is the main entry point for the napari plugin."""
+from polarityjam.napari_plugin.startup import run_napari
 
 
 def main():
-    startup()
+    """Run the napari plugin."""
+    run_napari("")
 
 
 if __name__ == "__main__":
-    startup()
+    main()
